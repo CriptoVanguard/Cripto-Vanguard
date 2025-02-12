9763feca5a58eae82f34d7aa50c7456f3a7a2ad4
InvestimentSecurity <investimentscrt@gmail.com>
2025-02-12 20:05:50 +0000

    Resolvendo conflito e finalizando o merge

--- cadastro/backend/server_cadastro.js
+++ cadastro/backend/server_cadastro.js
@@ -1,4 +1,5 @@
-require('dotenv').config(); // Carrega as variáveis de ambiente do arquivo .env
+require('dotenv').config({ path: '/Users/programacao/Documents/Cripto_vanguard/cadastro/.env' });
+console.log(process.env.DB_HOST); // Verifica se a variável DB_HOST foi carregada corretamente
 const express = require('express');
 const { Pool } = require('pg');
 const cors = require('cors');
@@ -12,13 +13,16 @@ const pool = new Pool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: 5432,  // A porta padrão do PostgreSQL
+    port: process.env.DB_PORT || 5432,  // Use a variável DB_PORT se definida, ou 5432 por padrão
 });
 
 // Conexão com o banco de dados para testar
 pool.connect()
     .then(() => console.log('Conectado ao banco de dados!'))
-    .catch(err => console.error('Erro ao conectar ao banco de dados', err.stack));
+    .catch(err => {
+        console.error('Erro ao conectar ao banco de dados', err.stack);
+        process.exit(1); // Encerra o servidor caso a conexão falhe
+    });
 
 // Middleware
 app.use(express.json());
@@ -28,6 +32,10 @@ app.use(cors());
 app.post('/cadastro', async (req, res) => {
     const { nome, email, senha } = req.body;
     
+    if (!nome || !email || !senha) {
+        return res.status(400).json({ message: 'Nome, email e senha são obrigatórios.' });
+    }
+
     // Inserir no banco de dados
     try {
         const query = 'INSERT INTO usuarios (nome, email, senha) VALUES ($1, $2, $3)';
