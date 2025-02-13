57dd87a19c5945f6d4227c9a582d7fa09dbd49cd
InvestimentSecurity <investimentscrt@gmail.com>
2025-02-13 12:02:05 +0000

    Resolvendo conflito e finalizando o merge

--- cadastro/backend/email/sendEmail.js
+++ cadastro/backend/email/sendEmail.js
@@ -12,7 +12,6 @@ const transporter = nodemailer.createTransport({
     },
 });
 
-
 // Função para enviar o e-mail de verificação
 async function sendVerificationEmail(email, token) {
     const verificationLink = `https://criptovanguard.github.io/Cripto-Vanguard/login/verify.html?token=${token}`;
@@ -22,10 +21,16 @@ async function sendVerificationEmail(email, token) {
         to: email,
         subject: 'Verifique sua conta no Cripto Vanguard',
         html: `
-            <h2>Bem-vindo ao Cripto Vanguard!</h2>
-            <p>Por favor, clique no link abaixo para verificar sua conta:</p>
-            <a href="${verificationLink}">Verificar conta</a>
-            <p>Se você não solicitou este e-mail, ignore-o.</p>
+            <div style="font-family: 'Arial', sans-serif; background-color: #1e1e1e; color: #fff; padding: 30px; text-align: center;">
+                <h1 style="color: #ffd700; font-size: 40px;">Cripto Vanguard</h1>
+                <h2 style="font-size: 24px;">Bem-vindo ao futuro das criptomoedas!</h2>
+                <p style="font-size: 18px;">Por favor, clique no botão abaixo para verificar sua conta e começar a negociar com segurança.</p>
+                <a href="${verificationLink}" style="display: inline-block; background-color: #ffd700; color: #1e1e1e; padding: 15px 30px; font-size: 20px; font-weight: bold; text-decoration: none; border-radius: 5px; margin-top: 20px;">Verificar Conta</a>
+                <p style="font-size: 14px; color: #ddd; margin-top: 30px;">Se você não solicitou este e-mail, por favor, ignore-o.</p>
+                <div style="margin-top: 50px; font-size: 12px; color: #bbb;">
+                    <p>Cripto Vanguard &copy; 2025 | Todos os direitos reservados.</p>
+                </div>
+            </div>
         `,
     };
 
